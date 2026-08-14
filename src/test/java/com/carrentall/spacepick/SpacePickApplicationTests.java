5993921ade3f6fc0275050c55191228939561d3d
jkjk9481-max <GitHub jkjk9481@kyungmin.ac.kr>
2026-08-14 17:40:42 +0900

    Initial SpacePick project

---
+++ src/test/java/com/carrentall/spacepick/SpacePickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrentall.spacepick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacePickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
